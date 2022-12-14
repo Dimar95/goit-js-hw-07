8e368227127d9b1d31e8c0b0609e96d3247c3c97
Kornilov <110562214+Dimar95@users.noreply.github.com>
2022-12-14 17:01:39 +0200

    gallery - 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery')
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+function onMarkupGallery(imgArray) {
+    galleryRef.innerHTML = imgArray.map(img => `<a class="gallery__link" href="${img.original}">
+    <img class="gallery__image" data-source="large-image.jpg" src="${img.preview}" alt="${img.description}"></a>`).join('');
+}
+
+function currentImg(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return
+    }
+   
+    const imgOriginalLink = e.target.parentNode.href;
+
+    const instance = basicLightbox.create(`
+    <div class="modal">
+    <img class="gallery__image" data-source="large-image.jpg" src="${imgOriginalLink}" alt="${e.target.alt}">
+    </div>
+`)
+        instance.show();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.querySelector('.gallery__link').href
+galleryRef.addEventListener('click', currentImg)
+onMarkupGallery(galleryItems)
+
+
+// event.preventDefault();
