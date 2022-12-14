6114a06ebc397f3d649f8434891b54e8a0c2471e
Kornilov <110562214+Dimar95@users.noreply.github.com>
2022-12-14 18:46:39 +0200

    tast-2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery')
 
-console.log(galleryItems);
+
+// console.log(galleryItems);
+onMarkupGallery(galleryItems)
+function onMarkupGallery(imgArray) {
+    galleryRef.innerHTML = imgArray.map(img => `<a class="gallery__item" href="${img.original}">
+  <img class="gallery__image" src="${img.preview}" alt="${img.description}" title=""/>
+</a>`).join('');
+        var lightbox = new SimpleLightbox('.gallery a', {
+        captionDelay: 250,
+        captions: true,
+        captionsData: 'alt'
+    });
+}
+function currentImg(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return
+    }
+    const imgOriginalLink = e.target.parentNode.href;
+
+}
+
+
+
+galleryRef.addEventListener('click', currentImg)
