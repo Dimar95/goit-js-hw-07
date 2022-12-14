8298d50ebcf6c76fe3003703d1996530ba5165b3
Kornilov <110562214+Dimar95@users.noreply.github.com>
2022-12-14 17:37:23 +0200

    esc tack - 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,8 +2,6 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryRef = document.querySelector('.gallery')
 
-// console.log(galleryItems);
-
 function onMarkupGallery(imgArray) {
     galleryRef.innerHTML = imgArray.map(img => `<a class="gallery__link" href="${img.original}">
     <img class="gallery__image" data-source="large-image.jpg" src="${img.preview}" alt="${img.description}"></a>`).join('');
@@ -14,36 +12,20 @@ function currentImg(e) {
     if (e.target.nodeName !== 'IMG') {
         return
     }
-   
     const imgOriginalLink = e.target.parentNode.href;
-
     const instance = basicLightbox.create(`
     <div class="modal">
     <img class="gallery__image" data-source="large-image.jpg" src="${imgOriginalLink}" alt="${e.target.alt}">
-    </div>
-`)
-        instance.show();
+    </div>`);
+    instance.show();
+    window.addEventListener('keydown', e => {
+        
+        if (e.code === 'Escape'){instance.close()}
+    })
+    
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// document.querySelector('.gallery__link').href
 galleryRef.addEventListener('click', currentImg)
 onMarkupGallery(galleryItems)
 
 
-// event.preventDefault();
